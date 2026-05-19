ac275dc05bcc8ab07d85af4ef20c5c9cba823aec
Youngmin322 <choyoungmin0322@gmail.com>
2026-05-19 16:33:10 +0900

    Initial Commit

---
+++ ConcurrencyImageGallery/ConcurrencyImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyImageGalleryApp.swift
+//  ConcurrencyImageGallery
+//
+//  Created by Youngmin Cho on 5/19/26.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
